49574f0dd2f9bc6e0436dfea65eb54f817bad754
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2022-03-23 00:30:01 +0530

    added repo func in app.js

--- githubexp/src/App.js
+++ githubexp/src/App.js
@@ -7,58 +7,90 @@ export default class App extends React.Component{
 
   state = {
     user: null,
-    error: null,
+    repos: [],
+    userDataError: null,
+    repoDataError: null,
     loading: false
   }
 
   fetchUserData = async (username) => {
-    // the code is placed in callback as we are not sure the setState function is async or not
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    if(res.ok){
+      const data = await res.json();
+      /*console.log(data);
+      return this.setState({
+        user: data,
+        loading: false
+      });*/
+      return {data};
+    }
+    else{
+      const error = (await res.json()).message;
+      /*this.setState({
+        error:error,
+        loading: false
+      });*/
+      return {error};
+    }
+  }
+
+  fetchRepoData = async (username) => {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?page=1`);
+    if(res.ok){
+      const data = await res.json();
+      return {data};
+    }
+    else{
+      const error = (await res.json()).message;
+      return {error};
+    }
+  }
+
+  fetchData = async (username) => {
+    // the code is placed in callback, as we are not sure the setState function will be async or not
     this.setState({
       loading: true
     }, async () => {
         //alert("Hi! " + username);
-        const res = await fetch(`https://api.github.com/users/${username}`);
         try{
-          if(res.ok){
-            const data = await res.json();
-            console.log(data);
+          const [user,repos] = await Promise.all([
+            this.fetchUserData(username),
+            this.fetchRepoData(username)
+          ]);
+          // const {data, error} = await this.fetchUserData(username);
+          if(user.data !== undefined && repos.data !==undefined)
             return this.setState({
-              user: data,
+              user: user.data,
+              repos: repos.data,
               loading: false
             });
-          }
-          else{
-            const error = (await res.json()).message;
-            this.setState({
-              error:error,
-              loading: false
-            });
-          }
+          this.setState({
+            userDataError: user.error,
+            repoDataError: repos.error,
+            loading: false
+          });
         }
         catch(err){
           console.log(err);
           this.setState({
-            error:'There has been an error!',
+            userDataError: 'There has been an error on user data side!',
+            repoDataError: 'There has been an error on repo data side!',
             loading: false
           });
         }
-        });
+      });
   }
 
   render(){
-    const {loading, error, user} = this.state;
+    const {loading, userDataError, repoDataError, user, repos} = this.state;
     return (
       <div className="App">
         <br/><br/><b><em><u><h1>Github Fetch User</h1></u></em></b><br/><br/>
-        <Search fetchData = {this.fetchUserData}/>
-        <br/><br/>
+        <Search fetchData = {this.fetchData}/><br/><br/>
         {loading && <p className='text-primary'><b><em>Loading ...</em></b></p>}
-        {error && 
-          <p className='text-danger'><b><em>{error}</em></b></p>
-        }
-        {!loading && !error && user && 
-          <UserCard user={user}/>
-        }
+        {userDataError && <p className='text-danger'><b><em>{userDataError}</em></b></p>}
+        {!loading && !userDataError && user && <UserCard user={user}/>}
+        {repoDataError && <p className='text-danger'><b><em>{repoDataError}</em></b></p>}
       </div>
     );
   }
--- githubexp/src/components/UserCard.js
+++ githubexp/src/components/UserCard.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 export default class UserCard extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
     render(){
         const user = this.props.user;
         return(
             <div>
                 <div className="container card">
                     <div className="cardBody">
-                        <h3>{user.name}</h3>
-                        <h3>{user.blog}</h3>
-                        <h3>{user.company}</h3>
-                        <h3>{user.location}</h3>
+                    <div className="d-flex">
+                    <img className="img-round user-image mr-5" height={100} src={user.avatar_url} />
+                    <div>
+                        <h5>{user.name}</h5>
+                        <p className="mb-0 font-italic">{user.company}</p>
+                        <p className="mb-0">{user.location}</p>
+                    </div>
+                    </div>
                     </div>
                 </div>
             </div>
